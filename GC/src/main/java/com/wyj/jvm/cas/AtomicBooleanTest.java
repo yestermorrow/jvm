167a32580360659f15ad710e2dbe4b0734191b91
yueju <yueju@2dfire.com>
2018-09-08 19:52:10 +0800

    compareAndSet(true,false)锁机制

---
+++ GC/src/main/java/com/wyj/jvm/cas/AtomicBooleanTest.java
@@ -0,0 +1,41 @@
+package com.wyj.jvm.cas;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * Cteated by wyj on 2017/9/9
+ */
+public class AtomicBooleanTest implements Runnable{
+
+    private static AtomicBoolean flag = new AtomicBoolean(true);
+
+    public static void main(String[] args) {
+        AtomicBooleanTest ast = new AtomicBooleanTest();
+        Thread thread1 = new Thread(ast);
+        Thread thread = new Thread(ast);
+        thread1.start();
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+        System.out.println("thread:" + Thread.currentThread().getName()+";flag:"+flag.get());
+        if (flag.compareAndSet(true, false)) {
+            System.out.println(Thread.currentThread().getName()+""+flag.get());
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            flag.set(true);
+        } else {
+            System.out.println("重试机制Thread："+Thread.currentThread().getName()+";flag"+flag.get());
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            run();
+        }
+    }
+}
